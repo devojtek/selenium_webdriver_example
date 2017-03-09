e402f220f655af733e3b687e580f2e8b9b4dc4c2
wlabas <wlabas@hotmail.com>
2017-03-09 08:32:06 +0100

    first commit

---
+++ selenium_webdriver_example/Program.cs
@@ -0,0 +1,65 @@
+﻿using Nito.AsyncEx;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace selenium_webdriver_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            chromedriver_appraisalitem.CreateNewAsync("http://localhost:8109/Admin/ElevatoSoftware/Appraisals/AppraisalItemSelfRating.aspx?PerformanceAppraisalsItemId=97", "wl+hra@44group.com", "test");
+            chromedriver_appraisalitem.CreateNewAsync("http://localhost:8109/Admin/ElevatoSoftware/Appraisals/AppraisalItemSelfRating.aspx?PerformanceAppraisalsItemId=98", "wl+hra@44group.com", "test");
+
+            //elevato_appraisalRatings();
+
+            Console.ReadKey();
+        }
+
+        private static async Task elevato_appraisalRatings()
+        {
+            //IWebDriver driver = new FirefoxDriver(FirefoxDriverService.CreateDefaultService(), new FirefoxOptions() { UseLegacyImplementation = true }, TimeSpan.FromSeconds(10));
+
+            // todo: implement firefox, ie... etc.
+            IWebDriver driver = new ChromeDriver();
+
+            // todo: implement url attribute
+            driver.Url = "http://localhost:8109/login.aspx?ReturnUrl=/admin";
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            driver
+                .FindElement(By.XPath("//*[@id='ctl00_ContentPlaceHolder1_lvLogin_EcommLogin1_Login1_UserName']"))
+                .SendKeys("wl+hra@44group.com");
+
+            driver
+                .FindElement(By.XPath("//*[@id='ctl00_ContentPlaceHolder1_lvLogin_EcommLogin1_Login1_Password']"))
+                .SendKeys("test" + Keys.Enter);
+
+            driver.Navigate().GoToUrl("http://localhost:8109/Admin/ElevatoSoftware/Appraisals/default.aspx");
+
+            driver.FindElement(By.XPath("//*[@id='ctl00_ContentPlaceHolder1_ShortCuts1_DivAppraisalManage']/a[1]")).Click();
+
+            driver.FindElement(By.XPath("//*[@id='ctl00_ContentPlaceHolder1_AppraisalsManage1_GvAppraisalsList']/tbody/tr[2]/td[1]/span/a")).Click();
+
+            var ratingsLinks = driver.FindElements(By.XPath("//td[@class='CommandColumn device_sm toSideBar']/div[@class='appraisalActionButtons']/a"));
+
+            foreach (var ratingLink in ratingsLinks)
+            {
+                string link = ratingLink.GetAttribute("href");
+                chromedriver_appraisalitem.CreateNewAsync(link, "wl+hra@44group.com", "test");
+
+                //driver.SwitchTo().Window(selfRatingLink.GetAttribute("href")).Url = selfRatingLink.GetAttribute("href");
+            }
+        }
+
+    }
+}
---
+++ selenium_webdriver_example/chromedriver_appraisalitem.cs
@@ -0,0 +1,86 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace selenium_webdriver_example
+{
+    public static class chromedriver_appraisalitem
+    {
+        public static async Task CreateNewAsync(string ratingLink, string login, string pass)
+        {
+            await Task.Factory.StartNew(() => CreateNew(ratingLink, login, pass));
+            
+        }
+
+        private static void CreateNew(string ratingLink, string login, string pass)
+        {
+            var ratingDriver = new ChromeDriver();
+            ratingDriver.Url = ratingLink;
+
+            var wait = new WebDriverWait(ratingDriver, TimeSpan.FromSeconds(2));
+
+            ratingDriver
+            .FindElement(By.XPath("//*[@id='ctl00_ContentPlaceHolder1_lvLogin_EcommLogin1_Login1_UserName']"))
+            .SendKeys(login);
+
+            ratingDriver
+                .FindElement(By.XPath("//*[@id='ctl00_ContentPlaceHolder1_lvLogin_EcommLogin1_Login1_Password']"))
+                .SendKeys(pass + Keys.Enter);
+
+            Task.Delay(TimeSpan.FromSeconds(1));
+            var cookieModal = ratingDriver.FindElementById("ctl00_ContentPlaceHolder1_AppraisalItemRating1_ReminderInformationCookieModalPopup");
+            if (cookieModal != null && cookieModal.Displayed)
+            {
+                cookieModal.FindElement(By.Id("ctl00_ContentPlaceHolder1_AppraisalItemRating1_LbReminderInformationSetCookie")).Click();
+            }
+
+            Task.Delay(TimeSpan.FromSeconds(1));
+
+            var content_section = ratingDriver
+            .FindElementByXPath("//div[@id='BusinessGoalPlansDiv']/div[@class='List-block']")
+            .FindElement(By.XPath("//div[@class='Content-section']"));
+
+
+            var selects = content_section
+                .FindElements(By.XPath("//div[@class='AppraisalItemSelectedGoalRating']/select"));
+            foreach (var select in selects)
+            {
+                var drpGoalRating = new SelectElement(select);
+
+                // todo: implement random
+                drpGoalRating.SelectByIndex(3);
+            }
+
+
+            //Thread.Sleep(TimeSpan.FromSeconds(2));
+            //var arrows = ratingDriver.FindElements(By.XPath("//div[@class='col-xs-2 col-sm-2 col-lg-1 arrowFixedPos']"));
+
+            //for (int i = arrows.Count - 1; i >= 0; i--)
+            //{
+            //    wait.Until<IWebElement>((d) => d.FindElement(By.CssSelector("btn-collapse"))).Click();
+            //}
+
+            Task.Delay(TimeSpan.FromSeconds(3));
+
+            var comments = ratingDriver.FindElementsByCssSelector("textarea[placeholder='Komentarz']");
+            foreach (var comment in comments)
+            {
+                if (comment.Displayed)
+                {
+                    comment.SendKeys("komentarz testowy"); 
+                }
+            }
+
+            Task.Delay(TimeSpan.FromSeconds(1));
+            ratingDriver.FindElementByXPath("//input[@type='submit']").Click();
+
+        }
+
+    }
+}
